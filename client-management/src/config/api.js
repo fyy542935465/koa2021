eb728269e417e98f8c8110abb4480624ee578aba
fyy542935465 <2039915126@qq.com>
2021-08-02 10:52:20 +0800

    s

--- client-management/src/config/api.js
+++ client-management/src/config/api.js
@@ -3,12 +3,12 @@ import CONFIG from './index'
 const CURRENT = CONFIG[ENV.CURRENT]
 const url = CURRENT.baseUrl
 export default {
-    // 保存产品
+    // 保存产品、编辑产品
     saveProduct:url + 'product/save',
     // 获取产品列表
     getProduct:url + 'product/find',
-    // 编辑产品
+    // 根据id获取产品详情
     findProductById:(id) => url + 'product/findByid/' + id,
-    // 编辑产品
-    findNewsById:(id) => url + 'product/findByid/' + id,
+    // 根据id获取新闻详情
+    findNewsById:(id) => url + 'news/findByid/' + id,
 }
